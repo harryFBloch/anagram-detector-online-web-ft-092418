703765fe522015151192fb3909b7ddfeec6eb507
Harry Bloch <harryfbloch@gmail.com>
2018-10-02 19:01:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,15 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :
-  def initialize
-    
+  attr_accessor :gram
+  def initialize(gram)
+    @gram = gram
+  end
+  
+  def match(word_array)
+    word_array.each {|element| 
+      sorted1 = element.sort()
+      sorted2 = @gram.sort()
+      do return element if sorted1 == sorted2
+    }
   end
 end
